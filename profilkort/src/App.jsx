10f40f21e59ddae61ed8c50013b88f6eae7b5526
Benjamav <benjamav@hiof.no>
2025-04-22 00:34:17 +0200

    profilkort
    opprettet profilkort

---
+++ profilkort/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Profilkort from "./profilkort";
+
+function App() {
+  return (
+    <Profilkort
+      name="Benjamin Vestlund"
+      email="Benjamav@hiof.no"
+      imageUrl= "https://cdn.discordapp.com/attachments/255713586452758541/1364000380714749962/IMG_1437.jpg?ex=680813d8&is=6806c258&hm=d6481f6398b957533ffed131e21549af3c3aaeadb67ad7f5b3f73031d4e8ee2c&"
+    />
+  );
+}
+
+export default App;
---
+++ profilkort/src/profilkort.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./profilkort.css";
+
+const ProfileCard = ({ name, email, imageUrl }) => {
+  return (
+    <div className="profile-card">
+      <img className="profile-image" src={imageUrl} alt={`${name}'s profile`} />
+      <h2 className="profile-name">{name}</h2>
+      <p className="profile-email">{email}</p>
+    </div>
+  );
+};
+
+export default ProfileCard;
